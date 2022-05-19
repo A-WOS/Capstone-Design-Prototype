69e1ed514ab6c06bc88c4f9d76db082b64015c0f
ehreh8404 <75967116+ehreh8404@users.noreply.github.com>
2022-05-20 05:02:54 +0900

    play admin 페이지 구현 및 임시 DB생성

--- play/admin.py
+++ play/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Player, Quiz
 
+class PlayerAdmin(admin.ModelAdmin):
+    search_fields = ['player_name']
+
+admin.site.register(Player,PlayerAdmin)
+
+class QuizAdmin(admin.ModelAdmin):
+    search_fields = ['quiz_title']
+
+admin.site.register(Quiz,QuizAdmin)
 # Register your models here.
--- play/models.py
+++ play/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    quiz_title = models.CharField(max_length=200)
+    quiz_Alist = models.CharField(max_length=200)
+    quiz_answer = models.IntegerField(default=0)
+    image = models.ImageField(blank=True)
+
+class Player(models.Model):
+    player_name = models.CharField(max_length=200)
+    player_score = models.IntegerField(default=0)
+
+
