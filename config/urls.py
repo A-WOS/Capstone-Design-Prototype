de717b2dbf2542c7f107824a87b66b1ec721245f
AWOS <uws0331@gmail.com>
2022-05-20 13:47:56 +0900

    유저 생성 페이지와 방목록

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from quiz import views
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', include('quiz.urls'))
+    path('quiz/', include('quiz.urls')),
+    path('', views.signup_login, name='signup'),
 ]
