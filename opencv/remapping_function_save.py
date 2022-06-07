588193fc5a540effbc4d09bb457fa06ca630383c
cgj10 <cgj1000@naver.com>
2022-06-07 23:19:53 +0900

    렌즈처리, 함수처리(sin, cos) 세이브

---
+++ opencv/remapping_function_save.py
@@ -0,0 +1,43 @@
+import glob
+
+import cv2
+import numpy as np
+
+
+file_list = glob.glob('.\\img\\*.jpg')
+img_name_list = []
+count = 0
+for i in file_list:
+    print(i)
+    img_name_list.append(i[6:])
+    img = cv2.imread(i)
+    l = 20      # 파장(wave length)
+    amp = 15    # 진폭(amplitude)
+
+    # img = cv2.imread('./img/elephant.jpg')
+    rows, cols = img.shape[:2]
+
+    # 초기 매핑 배열 생성 ---①
+    mapy, mapx = np.indices((rows, cols),dtype=np.float32)
+
+    # sin, cos 함수를 적용한 변형 매핑 연산 ---②
+    sinx = mapx + amp * np.sin(mapy/l)
+    cosy = mapy + amp * np.cos(mapx/l)
+
+    # 영상 매핑 ---③
+
+    img_sinx=cv2.remap(img, sinx, mapy, cv2.INTER_LINEAR) # x축만 sin 곡선 적용
+    img_cosy=cv2.remap(img, mapx, cosy, cv2.INTER_LINEAR) # y축만 cos 곡선 적용
+    # x,y 축 모두 sin, cos 곡선 적용 및 외곽 영역 보정
+    img_both=cv2.remap(img, sinx, cosy, cv2.INTER_CUBIC, \
+                        None, cv2.BORDER_REPLICATE)
+    # 결과 출력
+    # cv2.imshow('origin', img)
+    # cv2.imshow('sin x', img_sinx)
+    # cv2.imshow('cos y', img_cosy)
+    # cv2.imshow('sin cos', img_both)
+    cv2.imwrite('./remapping_img/remapping_{0}'.format(img_name_list[count]), img_both)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    count += 1
+
