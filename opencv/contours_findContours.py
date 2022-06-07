b4d77fb2e6594af5e6252633b93f12a37ba438fc
cgj10 <cgj1000@naver.com>
2022-06-07 23:15:47 +0900

    canny엣지, findContours 세이브 기능 구현 및 코드 수정

--- opencv/contours_findContours.py
+++ opencv/contours_findContours.py
@@ -16,6 +16,30 @@ binary = cv2.bitwise_not(binary)
 # RETR_EXTERNAL 최와곽 라인만 찾음
 contours, hierachy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+contours_data = np.array(contours)
+contours_data.shape
+
+value = list()
+for i in range(len(contours_data)):
+    for j in range(len(contours_data[i])):
+        value.append(contours_data[i][j][0][0])  # 네번째 괄호가 0일때 x의 값
+        x_min = min(value)
+        x_max = max(value)
+print(x_min)
+print(x_max)
+
+# y의 min과 max 찾기
+y_min, y_max = 0, 0
+value = list()
+for i in range(len(contours_data)):
+    for j in range(len(contours_data[i])):
+        value.append(contours_data[i][j][0][1])  # 네번째 괄호가 0일때 x의 값
+        y_min = min(value)
+        y_max = max(value)
+print(y_min)
+print(y_max)
+data_field = img[y_min:y_min+y_max, x_min:x_min+x_max]
+
 # 외곽선 그리기
 cv2.drawContours(black, contours, -1, (0, 0, 255), 2)
 cv2.drawContours(white, contours, -1, (0, 0, 255), 2)
@@ -26,6 +50,11 @@ cv2.imshow("gray", gray)
 # 검정부분
 cv2.imshow("black", black)
 cv2.imshow("white", white)
+cv2.imshow("binary", 255-binary)
+
+# 원하는 부분만 잘라보려 했는데 외곽선 부분만 자르는건 안되더라
+# contours를 담는 최소 크기의 사각형 필드
+cv2.imshow("data_field", data_field)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
