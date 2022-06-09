c9f01b2d923aef667e4cafd698631fbf721cf52d
AWOS <uws0331@gmail.com>
2022-06-10 04:59:20 +0900

    feat: 채팅 완성

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # room
     path('', views.signup_login, name='signup'),
+    path('logout/', views.remove_account, name='logout'),
     path('rooms/', views.index, name="rooms"),
     path('create_room/', views.create_room, name='create_room'),
     path('in_room/<int:room_id>/', views.in_room, name='in_room'),
 
-    # path('send', views.send, name='send'),
-    # path('getMessages/<int:room_id>/', views.getMessages, name='getMessages'),
+    path('send', views.send, name='send'),
+    path('getMessages/<int:room_id>/', views.getMessages, name='getMessages'),
     # path('getMessages/', views.getMessages, name='getMessages'),
     # path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,5 @@
 import django.contrib.auth.models
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import QuizRoom, QuizPlayers, Message
@@ -8,12 +8,18 @@ from .forms import PlayerForm, RoomForm
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('signup')
+
     room_list = QuizRoom.objects.all()
     context = {'room_list': room_list}
     return render(request, 'quiz/room_list.html', context)
 
 
 def signup_login(request):
+    if request.user.is_authenticated:
+        return redirect('rooms')
+
     if request.method == "POST":
         form = PlayerForm(request.POST)
         if form.is_valid():
@@ -30,7 +36,19 @@ def signup_login(request):
     return render(request, 'quiz/signup_login.html', {'form': form})
 
 
+def remove_account(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+
+    logout(request)
+
+    return redirect('signup')
+
+
 def create_room(request):
+    if not request.user.is_authenticated:
+        return redirect('signup')
+
     if request.method == "POST":
         form = RoomForm(request.POST)
         # print(form.fields[str('room_name')])
@@ -50,6 +68,9 @@ def create_room(request):
 # room.room_user = str()
 # request.user = SimpleLazyObject
 def in_room(request, room_id):
+    if not request.user.is_authenticated:
+        return redirect('signup')
+
     room = get_object_or_404(QuizRoom, pk=room_id)
     print(f'room type : {room, type(room), room.id}')
     # room_details = QuizRoom.objects.get(room_name=room)
@@ -66,22 +87,21 @@ def in_room(request, room_id):
     return render(request, 'quiz/in_room.html', context)
 
 
-# def send(request):
-#     message = request.POST['message']
-#     username = request.POST['username']
-#     room_id = request.POST['room_id']
-#     print(message)
-#     print(f'send : {message, username, room_id}')
-#     new_message = Message.objects.create(value=message, user=username, room=room_id)
-#     new_message.save()
-#     return HttpResponse('메시지 보내기 성공')
-#
-#
-# def getMessages(request, room_id):
-#     # messages = Message.objects.filter(room=room_id)
-#     messages = Message.objects.get(room=room_id)
-#     print(f'getMessages : {messages}')
-#     return JsonResponse({"messages": list(messages.values())})
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+    print(message)
+    print(f'send : {message, username, room_id}')
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('메시지 보내기 성공')
+
+
+def getMessages(request, room_id):
+    messages = Message.objects.filter(room=room_id)
+    # print(f'getMessages : {messages}')
+    return JsonResponse({"messages": list(messages.values())})
 
 
 # def delete_user():
