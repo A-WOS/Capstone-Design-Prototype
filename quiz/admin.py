8081860a34a5ee4336157f6f92d41912076fc3c5
AWOS <uws0331@gmail.com>
2022-06-09 12:05:06 +0900

    admin 페이지에 Message 추가

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import QuizRoom, QuizPlayers
+from .models import QuizRoom, QuizPlayers, Message
 
 
 class QuizRoomAdmin(admin.ModelAdmin):
@@ -10,5 +10,10 @@ class QuizPlayersAdmin(admin.ModelAdmin):
     search_fields = ['username']
 
 
+class MessageAdmin(admin.ModelAdmin):
+    search_fields = ['value']
+
+
 admin.site.register(QuizRoom, QuizRoomAdmin)
 admin.site.register(QuizPlayers, QuizPlayersAdmin)
+admin.site.register(Message, MessageAdmin)
