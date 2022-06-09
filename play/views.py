e01957552869d644261f65064e43b69b7eb5c161
AWOS <uws0331@gmail.com>
2022-06-10 01:29:03 +0900

    모델 및 merge 변경사항 적용

--- play/views.py
+++ play/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+
+from quiz.models import QuizPlayers
 from .models import Quiz, Player
 from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     quiz = Quiz.objects.get(id=1)
-    player = Player.objects.get(id=request.user.id)
+    # player = Player.objects.get(id=request.user.id)
+    player = QuizPlayers.objects.get(id=request.user.id)
+    print(player)
     player.player_score = 0
     player.save()
     return render(request, 'play/play_round.html', {'quiz': quiz})
-
 
 
 def play_round_after(request):
@@ -18,25 +21,26 @@ def play_round_after(request):
     print(type(U_ans))
     quiz = Quiz.objects.get(id=round)
     print(type(quiz.quiz_answer))
-    player = Player.objects.get(id=request.user.id)
-    if(quiz.quiz_answer == int(U_ans)):
+    player = QuizPlayers.objects.get(id=request.user.id)
+    # player = Player.objects.get(id=request.user.id)
+    if (quiz.quiz_answer == int(U_ans)):
         player.player_score += 10
         player.save()
 
+    return render(request, 'play/play_round_after.html', {'quiz': quiz, 'U_ans': int(U_ans), 'player': player})
 
-
-    return render(request, 'play/play_round_after.html', {'quiz' : quiz,'U_ans':int(U_ans),'player':player})
 
 def play_round(request):
     round = request.POST['round']
 
-    quiz = Quiz.objects.get(id=int(round)+1)
+    quiz = Quiz.objects.get(id=int(round) + 1)
 
-    return render(request, 'play/play_round.html',{'quiz': quiz})
+    return render(request, 'play/play_round.html', {'quiz': quiz})
 
 
 def play_result(request):
-    player_list = Player.objects.order_by('id')
-    context = {'player_list': player_list}
+    # player_list = Player.objects.order_by('id')
+    player = QuizPlayers.objects.get(id=request.user.id)
+    context = {'player': player}
 
     return render(request, 'play/play_result.html', context)
